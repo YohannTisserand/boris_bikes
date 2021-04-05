93aaead16f137d09045c5437d3e522090a5d3cab
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-05 20:20:00 +0100

    rakefile added(typo)

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,7 @@
 require 'rspec/core/rake_task'
 require 'rake'
 
-Rspec::Core::RakeTask.new(:spec) do |task|
+RSpec::Core::RakeTask.new(:spec) do |task|
   task.pattern = Dir.glob('spec/**/*_spec.rb')
   task.rspec_opts - '--format documentation'
 end

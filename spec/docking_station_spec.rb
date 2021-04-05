cafb42db497fa8cf1dffa3d25774d44ffaae0720
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-05 23:25:09 +0100

    error raised when empty

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,24 +1,37 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
+  
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
   context '#release_bike' do
-    it 'should release working bikes' do
-      bike = subject.release_bike
-      expect(bike).to be_working
+    it { is_expected.to respond_to(:release_bike) }
+
+    it 'release working bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to be_working
+  end
+
+    it 'releases the bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when docking station is empty' do
+      expect { subject.release_bike }.to raise_error 'No bike available'
     end
   end
 
   context '#dock' do
-    it 'should dock a bike' do
+    it 'docks bike' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq(bike)
+      expect(subject.dock(bike)).to eq bike
     end
 
-    it 'should return docked bikes' do
+    it 'should return docked bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.bike).to eq bike
